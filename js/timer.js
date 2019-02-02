e3b0af9ea07d0b97f99f69b40d20275a770ed614
ObjectionTheory <therandomguy265@gmail.com>
2019-02-02 12:15:46 +0000

    Banner

--- js/timer.js
+++ js/timer.js
@@ -17,8 +17,6 @@ var x = setInterval(function() {
     // If the count down is finished, write some text 
     if (distance < 0) {
     clearInterval(x);
-    $("#time").html("🎉🎊🎉
-                    HAPPY BIRTHDAY LUCY!!
-                    🎊🎉🎊🎉");
+    $("#time").html("🎉🎊🎉 HAPPY BIRTHDAY LUCY!!! 🎉🎊🎉");
     }
 }, 150);
